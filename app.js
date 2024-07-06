ad76b0df18d3bc3a78930be9c917072f4819196d
Kate Roshchupkina <katerina.ermakov@mail.ru>
2024-07-06 14:29:14 +0300

    Corrected preliminary design of the project

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-// const { errors } = require("celebrate");
+const { errors } = require("celebrate");
 const helmet = require("helmet");
 const process = require("dotenv").config();
 
 const { MONGODB_URL_DEV, PORT_DEV } = require("./constants");
 const { PORT = PORT_DEV, MONGO_URL = MONGODB_URL_DEV } = process.env;
+
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const errorHandler = require("./middlewares/error-handler");
+const { limiter } = require("./middlewares/rate-limit");
 
 const app = express();
 app.use(helmet());
@@ -28,6 +32,23 @@ mongoose.connect(MONGO_URL).then(() => console.log("Connected!"));
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(requestLogger); // логгер запросов
+app.use(limiter);
+
+/////////////////////////////////////////
+
+////////////////////////////////////////
+
+app.use((req, res, next) => {
+  next(new NotFoundError("Страница не найдена"));
+});
+
+app.use(errorLogger); // логгер ошибок
+
+// обработчики ошибок
+app.use(errors()); // обработчик ошибок celebrate
+app.use(errorHandler); // централизованный обработчик
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
